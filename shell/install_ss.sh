66c94aca8970e663512445d8265c4a04e8458485
rogerluo <luodingjia1985@163.com>
2018-05-18 23:09:44 +0800

    fix port not found

--- shell/install_ss.sh
+++ shell/install_ss.sh
@@ -40,10 +40,21 @@ if [ $num -eq 0 ];then
 echo -e "\nsytemctl start supervisord.service" >> /etc/rc.local
 fi
 
-systemctl start supervisord.service
+systemctl start firewalld.service 
+
+systemctl stop supervisord.service
 
 firewall-cmd --zone=public --add-port=${server_port}/tcp --permanent
 
 firewall-cmd --reload
 
-echo "done"
+systemctl start supervisord.service
+
+sleep 1
+
+num=$(netstat -anplt |grep ${server_port} | grep -v grep | wc -l)
+if [ ${num} -eq 0 ];then
+    echo "not found port:${server_port} "
+else
+    echo "done"
+fi
