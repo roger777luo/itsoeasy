cbc5e53540a0f752f8656ce709c6d4f2fd9a387f
rogerluo <luodingjia1985@163.com>
2018-05-18 22:58:52 +0800

    fix sed bugs

--- shell/install_ss.sh
+++ shell/install_ss.sh
@@ -4,7 +4,7 @@ env_build () {
     yum install -y python-setuptools m2crypto supervisor
     yum install net-tools -y
     easy_install pip
-    pip install shadowsocks -y
+    pip install shadowsocks
 }
 
 # set password
@@ -17,12 +17,15 @@ method="aes-256-cfb"
 env_build
 
 cp shadowsocks.json target.json
-sed -i 's/#password/${password}/g' target.json
-sed -i 's/#server_port/${server_port}/g' target.json
-sed -i 's/#local_port/${local_port}/g' target.json
+sed -i "s/#password/${password}/g" target.json
+sed -i "s/#server_port/${server_port}/g" target.json
+sed -i "s/#local_port/${local_port}/g" target.json
 
 cp target.json /etc/shadowsocks.json
 
+num=$(grep shadowsocks /etc/supervisord.conf | grep -v grep | wc -l)
+
+if [ $num -eq 0 ];then
 echo -e "\n[program:shadowsocks]
 command=ssserver -c /etc/shadowsocks.json
 autostart=true
@@ -30,10 +33,14 @@ autorestart=true
 user=root
 log_stderr=true
 logfile=/var/log/shadowsocks.log" >> /etc/supervisord.conf
+fi
 
-echo -e "\nservice supervisord start|sytemctl start supervisord.service" >> /etc/rc.local
+num=$(grep supervisord /etc/rc.local | grep -v grep | wc -l)
+if [ $num -eq 0 ];then
+echo -e "\nsytemctl start supervisord.service" >> /etc/rc.local
+fi
 
-systemctl stop supervisord.service
+systemctl start supervisord.service
 
 firewall-cmd --zone=public --add-port=${server_port}/tcp --permanent
 
